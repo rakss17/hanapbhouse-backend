cccd5ee2d7f8100c26809e71ce6b3c4683585fc5
Bohari S. Ambulo <bohariambulo17@gmail.com>
2024-06-11 22:56:39 +0800

    sending and receiving messages in real-time in MessageConsumer

--- hanapbhouse/message/consumers.py
+++ hanapbhouse/message/consumers.py
@@ -2,6 +2,7 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from .models import Message
 from accounts.models import User
+from accounts.serializers import UserSerializer
 from channels.db import database_sync_to_async
 from .serializers import MessageSerializer
 from django.contrib.auth.models import AnonymousUser
@@ -52,44 +53,68 @@ class MessageConsumer(AsyncWebsocketConsumer):
         )
 
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        sender = text_data_json['sender']
-        receiver = text_data_json['receiver']
+        try:
+            text_data_json = json.loads(text_data)
+            message = text_data_json['message']
+            sender = text_data_json['sender']
+            receiver = text_data_json['receiver']
 
-        # Save message to database
-        sender_obj = User.objects.get(id=sender)
-        receiver_obj = User.objects.get(id=receiver)
-        message_obj = Message.objects.create(room_name=self.room_name, content=message, sender=sender_obj, receiver=receiver_obj)
+            # Save message to database
+            sender_obj = await self.get_sender_obj(sender)
+            receiver_obj = await self.get_receiver_obj(receiver)
+            message_obj = await self.create_message(self.room_name, message, sender_obj, receiver_obj)
 
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message,
-                'sender': sender,
-                'send_timestamp': message_obj.send_timestamp.isoformat(),
-                'receiver': receiver,
-                'read_timestamp': message_obj.read_timestamp.isoformat(),
-                'is_read_by_receiver': message_obj.is_read_by_receiver
-            }
-        )
+            # Send message to room group
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message_id': message_obj['id'],
+                    'room_name': message_obj['room_name'],
+                    'content': message_obj['content'], 
+                    'sender_id': message_obj['sender'],
+                    'sender_fullname': message_obj['sender_fullname'],
+                    'send_timestamp': message_obj['send_timestamp'],
+                    'receiver_id': message_obj['receiver'],
+                    'receiver_fullname': message_obj['receiver_fullname'],
+                    'read_timestamp': message_obj['read_timestamp'],
+                    'is_read_by_receiver': message_obj['is_read_by_receiver']
+                }
+            )
+
+        except Exception as e:
+            print(f"Error processing message: {e}")
+
+    @database_sync_to_async
+    def get_sender_obj(self, sender):
+        user_instance = User.objects.get(id=sender)
+        return user_instance
+    
+    @database_sync_to_async
+    def get_receiver_obj(self, receiver):
+        user_instance = User.objects.get(id=receiver)
+        return user_instance
+    
+    @database_sync_to_async
+    def create_message(self, room_name, message, sender_obj, receiver_obj):
+        queryset = Message.objects.create(room_name=room_name, content=message, sender=sender_obj, receiver=receiver_obj)
+        return MessageSerializer(queryset).data
 
     async def chat_message(self, event):
-        message = event['message']
-        sender = event['sender']
-        send_timestamp = event['send_timestamp']
-        receiver = event['receiver']
-        read_timestamp = event['read_timestamp']
-        is_read_by_receiver = event['is_read_by_receiver']
-
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message,
-            'sender': sender,
-            'send_timestamp': send_timestamp,
-            'receiver': receiver,
-            'read_timestamp': read_timestamp,
-            'is_read_by_receiver': is_read_by_receiver
-        }))
+        try:
+            # Send message to WebSocket
+            await self.send(text_data=json.dumps({
+                'message_id': event['message_id'],
+                'room_name': event['room_name'],
+                'content': event['content'], 
+                'sender_id': event['sender_id'],
+                'sender_fullname': event['sender_fullname'],
+                'send_timestamp': event['send_timestamp'],
+                'receiver_id': event['receiver_id'],
+                'receiver_fullname': event['receiver_fullname'],
+                'read_timestamp': event['read_timestamp'],
+                'is_read_by_receiver': event['is_read_by_receiver']
+            }))
+            
+        except Exception as e:
+            print(f"Error sending message: {e}")
