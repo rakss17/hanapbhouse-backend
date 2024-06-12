22090502f95438204bc3855506db6c599957ab8d
Bohari S. Ambulo <bohariambulo17@gmail.com>
2024-06-12 22:44:13 +0800

    initial read_timestamp and read by receiver updating data function

--- hanapbhouse/message/consumers.py
+++ hanapbhouse/message/consumers.py
@@ -2,7 +2,7 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from .models import Message
 from accounts.models import User
-from accounts.serializers import UserSerializer
+from django.utils import timezone
 from channels.db import database_sync_to_async
 from .serializers import MessageSerializer
 from django.contrib.auth.models import AnonymousUser
@@ -13,6 +13,8 @@ class MessageConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
 
+        self.user_id = self.scope['url_route']['kwargs']['user_id']
+        print("self.user_id", self.user_id)
         if isinstance(self.scope['user'], AnonymousUser):      
             await self.close()
         else:
@@ -21,29 +23,68 @@ class MessageConsumer(AsyncWebsocketConsumer):
                 self.room_group_name,
                 self.channel_name
             )
+            await self.channel_layer.group_add(self.user_id, self.channel_name)
             await self.accept()
-            
+
+            read_messages = await self.get_read_messages_by_receiver(self.user_id)
+            if read_messages is not None:
+                for message in read_messages:
+                    sender_user_id = message['sender']  # Ensure this is the correct field and type
+                    await self.channel_layer.group_send(
+                        sender_user_id,
+                        {
+                            'type': 'send_update_message',  # This is the default event type for sending WebSocket messages
+                            'action': 'read_message',
+                            'message_id': message['id'],
+                            'content': message['content'], 
+                            'read_timestamp': message['read_timestamp'],
+                            'is_read_by_receiver': message['is_read_by_receiver'],
+                            'sender_fullname': message['sender_fullname'],
+                            'receiver_fullname': message['receiver_fullname']
+                            
+                        }
+                    )
+
             # Send previous messages to WebSocket
-            messages = await self.get_messages(self.room_name)
+            messages = await self.get_messages(self.room_name, self.user_id)
       
             for message in messages:
-                await self.send(text_data=json.dumps({
-                    'message_id': message['id'],
-                    'room_name': message['room_name'],
-                    'content': message['content'], 
-                    'sender_id': message['sender'],
-                    'sender_fullname': message['sender_fullname'],
-                    'send_timestamp': message['send_timestamp'],
-                    'receiver_id': message['receiver'],
-                    'receiver_fullname': message['receiver_fullname'],
-                    'read_timestamp': message['read_timestamp'],
-                    'is_read_by_receiver': message['is_read_by_receiver']
-                }))
-
+                await self.channel_layer.group_send(
+                    self.room_group_name, 
+                    { 
+                        'type': 'chat_message',
+                        'message_id': message['id'],
+                        'room_name': message['room_name'],
+                        'content': message['content'], 
+                        'sender_id': message['sender'],
+                        'sender_fullname': message['sender_fullname'],
+                        'send_timestamp': message['send_timestamp'],
+                        'receiver_id': message['receiver'],
+                        'receiver_fullname': message['receiver_fullname'],
+                        'read_timestamp': message['read_timestamp'],
+                        'is_read_by_receiver': message['is_read_by_receiver']
+                    })
+                
     @database_sync_to_async
-    def get_messages(self, room_name):
-        queryset = Message.objects.filter(room_name=room_name).order_by('send_timestamp')
-        return MessageSerializer(queryset, many=True).data
+    def get_messages(self, room_name, user_id):
+        filtered_by_room_name = Message.objects.filter(room_name=room_name).order_by('send_timestamp')
+
+        return MessageSerializer(filtered_by_room_name, many=True).data
+    @database_sync_to_async
+    def get_read_messages_by_receiver(self, user_id):
+        time_now = timezone.localtime(timezone.now())
+        filtered_by_receiver = Message.objects.filter(receiver=user_id)
+        messages_data = []
+        for filtered in filtered_by_receiver:
+            if filtered.is_read_by_receiver == False and filtered.read_timestamp is None:
+                filtered.is_read_by_receiver = True
+                filtered.read_timestamp = time_now
+                filtered.save()
+                messages_data.append(MessageSerializer(filtered).data)
+
+        return messages_data
+            
+    
 
     async def disconnect(self, close_code):
         # Leave room group
@@ -53,37 +94,59 @@ class MessageConsumer(AsyncWebsocketConsumer):
         )
 
     async def receive(self, text_data):
-        try:
-            text_data_json = json.loads(text_data)
-            message = text_data_json['message']
-            sender = text_data_json['sender']
-            receiver = text_data_json['receiver']
+    
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        sender = text_data_json['sender']
+        receiver = text_data_json['receiver']
 
-            # Save message to database
-            sender_obj = await self.get_sender_obj(sender)
-            receiver_obj = await self.get_receiver_obj(receiver)
-            message_obj = await self.create_message(self.room_name, message, sender_obj, receiver_obj)
+        # Save message to database
+        sender_obj = await self.get_sender_obj(sender)
+        receiver_obj = await self.get_receiver_obj(receiver)
+        message_obj = await self.create_message(self.room_name, message, sender_obj, receiver_obj)
+        print("scope", self.scope['user'].id)
 
-            # Send message to room group
-            await self.channel_layer.group_send(
-                self.room_group_name,
-                {
-                    'type': 'chat_message',
-                    'message_id': message_obj['id'],
-                    'room_name': message_obj['room_name'],
-                    'content': message_obj['content'], 
-                    'sender_id': message_obj['sender'],
-                    'sender_fullname': message_obj['sender_fullname'],
-                    'send_timestamp': message_obj['send_timestamp'],
-                    'receiver_id': message_obj['receiver'],
-                    'receiver_fullname': message_obj['receiver_fullname'],
-                    'read_timestamp': message_obj['read_timestamp'],
-                    'is_read_by_receiver': message_obj['is_read_by_receiver']
-                }
-            )
+        read_messages = await self.get_read_messages_by_sender(receiver)
+        print("read_messages", read_messages)
+        if read_messages is not None:
+            for message in read_messages:
+                sender_user_id = message['sender']  # Ensure this is the correct field and type
+                await self.channel_layer.group_send(
+                    sender_user_id,
+                    {
+                        'type': 'send_update_message',  # This is the default event type for sending WebSocket messages
+                        'action': 'read_message',
+                        'message_id': message['id'],
+                        'content': message['content'], 
+                        'read_timestamp': message['read_timestamp'],
+                        'is_read_by_receiver': message['is_read_by_receiver'],
+                        'sender_fullname': message['sender_fullname'],
+                        'receiver_fullname': message['receiver_fullname']
+                        
+                    }
+                )
+        
+        message_obj_filtered_id = message_obj['id']
+        message_obj_filtered = await self.get_created_message_filtered(message_obj_filtered_id)
 
-        except Exception as e:
-            print(f"Error processing message: {e}")
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message_id': message_obj_filtered['id'],
+                'room_name': message_obj_filtered['room_name'],
+                'content': message_obj_filtered['content'], 
+                'sender_id': message_obj_filtered['sender'],
+                'sender_fullname': message_obj_filtered['sender_fullname'],
+                'send_timestamp': message_obj_filtered['send_timestamp'],
+                'receiver_id': message_obj_filtered['receiver'],
+                'receiver_fullname': message_obj_filtered['receiver_fullname'],
+                'read_timestamp': message_obj_filtered['read_timestamp'],
+                'is_read_by_receiver': message_obj_filtered['is_read_by_receiver']
+            }
+        )
+
+        
 
     @database_sync_to_async
     def get_sender_obj(self, sender):
@@ -99,6 +162,46 @@ class MessageConsumer(AsyncWebsocketConsumer):
     def create_message(self, room_name, message, sender_obj, receiver_obj):
         queryset = Message.objects.create(room_name=room_name, content=message, sender=sender_obj, receiver=receiver_obj)
         return MessageSerializer(queryset).data
+    
+    @database_sync_to_async
+    def get_read_messages_by_sender(self, user_id):
+        time_now = timezone.localtime(timezone.now())
+        filtered_by_receiver = Message.objects.filter(receiver=user_id)
+        messages_data = []
+        print("user_id", user_id)
+        for filtered in filtered_by_receiver:
+            if filtered.is_read_by_receiver == False and filtered.read_timestamp is None and user_id == user_id:
+                filtered.is_read_by_receiver = True
+                filtered.read_timestamp = time_now
+                filtered.save()
+                messages_data.append(MessageSerializer(filtered).data)
+
+        return messages_data
+    
+    @database_sync_to_async
+    def get_created_message_filtered(self, message_id):
+        filtered_by_room_name = Message.objects.filter(id=message_id).first()
+        print("filtered_by_room_name", filtered_by_room_name)
+        return MessageSerializer(filtered_by_room_name).data
+    
+    
+    
+    
+    async def send_update_message(self, event):
+        try:
+            # Send message to WebSocket
+            await self.send(text_data=json.dumps({
+                'action': event['action'],
+                'message_id': event['message_id'],  
+                'content': event['content'], 
+                'sender_fullname': event['sender_fullname'],
+                'receiver_fullname': event['receiver_fullname'],
+                'read_timestamp': event['read_timestamp'],
+                'is_read_by_receiver': event['is_read_by_receiver']
+            }))
+            
+        except Exception as e:
+            print(f"Error sending message: {e}")
 
     async def chat_message(self, event):
         try:
