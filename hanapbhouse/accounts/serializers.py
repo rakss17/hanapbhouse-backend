cb296ae5e81a90f8cc837caaba96ff0aa4d47ba6
Bohari S. Ambulo <bohariambulo17@gmail.com>
2024-06-16 23:12:30 +0800

    updating user profile details view and refactor user serializer

--- hanapbhouse/accounts/serializers.py
+++ hanapbhouse/accounts/serializers.py
@@ -2,28 +2,41 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
-    contact_number = serializers.IntegerField()
-    gender = serializers.CharField()
 
     class Meta:
         model = User
-        fields = ['id','username', 'email', 'first_name', 'last_name', 'password', 'contact_number', 'gender']
+        fields = ['id','username', 'email', 'first_name', 'last_name', 'password', 'contact_number', 'gender', 'image']
         extra_kwargs = {
             'password': {'write_only': True}
         }
         
-    def save(self, **kwargs):
-        user = User.objects.create_user(
-                username=self.validated_data['username'],
-                email=self.validated_data['email'],
-                first_name=self.validated_data['first_name'],
-                last_name=self.validated_data['last_name'],
-                password=self.validated_data['password'],
-                contact_number=self.validated_data['contact_number'],
-                gender=self.validated_data['gender'],
-            )
+    def create(self, validated_data):
+        username = validated_data.get('username')
+        email = validated_data.get('email')
+        first_name = validated_data.get('first_name')
+        last_name = validated_data.get('last_name')
+        password = validated_data.get('password')
+        contact_number = validated_data.get('contact_number')
+        gender = validated_data.get('gender')
 
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            contact_number=contact_number,
+            gender=gender
+        )
+        
         user.is_active = False
         user.save()
-
         return user
+
+    def update(self, instance, validated_data):
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
