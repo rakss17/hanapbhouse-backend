dea5175bd0d1c9513508f584e382ab147204d18d
Bohari S. Ambulo <bohariambulo17@gmail.com>
2024-06-17 22:41:31 +0800

    formatted date and time for serializers timestamp

--- hanapbhouse/feed/serializers.py
+++ hanapbhouse/feed/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Feed, SavedFeed
 from property.serializers import PropertySerializer
+from django.utils.timezone import localtime
+from utils.helpers import format_date_and_time
 
 class FeedSerializer(serializers.ModelSerializer):
     content = PropertySerializer()
@@ -20,6 +22,17 @@ class FeedSerializer(serializers.ModelSerializer):
             owner_image = owner.image.url
             return owner_image
         return None
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        
+        # Convert send_timestamp and read_timestamp to local timezone
+        time_local = localtime(instance.timestamp)
+        
+        serializer_type = "other"
+        representation['timestamp'] = format_date_and_time(time_local, serializer_type)
+        
+        return representation
 
     class Meta:
         model = Feed
@@ -50,6 +63,17 @@ class SavedFeedSerializer(serializers.ModelSerializer):
             owner_fullname = f'{owner.first_name} {owner.last_name}'
             return owner_fullname
         return None
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        
+        # Convert send_timestamp and read_timestamp to local timezone
+        time_local = localtime(instance.timestamp)
+        
+        serializer_type = "other"
+        representation['timestamp'] = format_date_and_time(time_local, serializer_type)
+        
+        return representation
     
     class Meta:
         model = SavedFeed
--- hanapbhouse/message/serializers.py
+++ hanapbhouse/message/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from .models import Message, UserChannelTracking
-from django.utils.timezone import now, localtime
+from django.utils.timezone import localtime
 from django.utils.translation import gettext_lazy as _
-from datetime import timedelta
+from utils.helpers import format_date_and_time
 
 class MessageSerializer(serializers.ModelSerializer):
     sender_fullname = serializers.SerializerMethodField()
@@ -33,37 +33,9 @@ class MessageSerializer(serializers.ModelSerializer):
         else:
             read_time_local = None
         
-        # Function to format time in 12-hour format
-        def format_time(datetime_obj):
-            return datetime_obj.strftime('%I:%M %p').lstrip('0').replace(' 0', ' ')
-        
-        # Function to format date and time based on conditions
-        def format_date_and_time(datetime_obj):
-            today = now().date()
-            start_of_current_year = today.replace(month=1, day=1)
-    
-            # Calculate the difference in days from today to the start of the current year
-            days_into_year = (today - start_of_current_year).days
-            
-            # Determine the date one year ago from today, considering the start of the year
-            not_current_year = today - timedelta(days=days_into_year + 1)
-            if datetime_obj and datetime_obj.date() == today:
-                return format_time(datetime_obj)  # Just the time for today
-            elif datetime_obj and datetime_obj >= now() - timedelta(days=7):  # Within the last 7 days
-                return f"{datetime_obj.strftime('%a')} at {format_time(datetime_obj)}"  # Day abbreviation and time
-            elif datetime_obj and datetime_obj.date() >= not_current_year:  # Within current year but more than 7 days ago
-                day = datetime_obj.day
-                formatted_day = str(day) if day > 9 else f'0{day}' 
-                return f"{datetime_obj.strftime('%b')} {formatted_day} at {format_time(datetime_obj)}"  # Month abbreviation, day, and time
-            else:
-                # Not current year or previous years, include the year in the format
-                if datetime_obj:
-                    day = datetime_obj.day
-                    formatted_day = str(day) if day > 9 else f'0{day}'
-                    return f"{datetime_obj.strftime('%b')} {formatted_day}, {datetime_obj.strftime('%Y')} at {format_time(datetime_obj)}"
-        
-        representation['send_timestamp'] = format_date_and_time(send_time_local)
-        representation['read_timestamp'] = format_date_and_time(read_time_local)
+        serializer_type = "message"
+        representation['send_timestamp'] = format_date_and_time(send_time_local, serializer_type)
+        representation['read_timestamp'] = format_date_and_time(read_time_local, serializer_type)
         
         return representation
 
