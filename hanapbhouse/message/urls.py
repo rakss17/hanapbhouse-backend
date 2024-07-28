37fad08a6025666a51781b48f2e7ce29cc683e52
Bohari S. Ambulo <bohariambulo17@gmail.com>
2024-07-28 13:45:06 +0800

    fixed typo conversation url name

--- hanapbhouse/message/urls.py
+++ hanapbhouse/message/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('coversation/', views.ConversationView.as_view(), name='coversation'),
+    path('conversation/', views.ConversationView.as_view(), name='conversation'),
     path('all-conversation/', views.AllConversationView.as_view(), name='all-conversation'),
 ]
